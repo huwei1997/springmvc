e47fa99a57a627e33efed6c1f1613f88d927e026
huwei1997 <1948202179@qq.com>
2018-09-04 10:20:54 +0800

    注释

--- springmvc1/src/main/java/cap/controller/HelloController.java
+++ springmvc1/src/main/java/cap/controller/HelloController.java
@@ -1,8 +1,14 @@
 package cap.controller;
 
+import com.sun.javafx.collections.MappingChange;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/")
 public class HelloController {
@@ -10,5 +16,25 @@ public class HelloController {
     public String test(Model model){
         model.addAttribute("data","SpringMVC");
         return "test";
+    }
+
+    @RequestMapping("/test2")
+    public void test2(Model model) {
+        model.addAttribute("data", "SpringMVC Test2");
+    }
+
+    @RequestMapping("/test3")
+    public ModelAndView test3() {
+        ModelAndView modelAndView = new ModelAndView("test3");
+        modelAndView.addObject("data", "SpringMVC test3");
+        return modelAndView;
+    }
+
+    @RequestMapping("/test4")
+    public Map test4() {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+        return map;
     }
 }
